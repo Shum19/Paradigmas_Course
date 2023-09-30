63207bb16a39bab476ed6bab012d3dd9923ecd7b
KseniiaS <myNew@email.com>
2023-09-30 10:32:19 +0300

    Home_Task_2

---
+++ src/main/java/Home_Task_2/Main.java
@@ -0,0 +1,7 @@
+package Home_Task_2;
+
+public class Main {
+    public static void main (String[] args){
+        MultiplicationTable.printMultiplyByOne(20);
+    }
+}
---
+++ src/main/java/Home_Task_2/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package Home_Task_2;
+
+public class MultiplicationTable {
+    public static void printMultiplyByOne(int num){
+        for (int i = 1; i <= num; i++) {
+            System.out.println("1 * " + i +" = " + 1 * i);
+        }
+    }
+}
